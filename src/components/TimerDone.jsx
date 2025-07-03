f9afb4cc873a2a015cb6612614c2ae1670765c06
Wafflegum <70809819+Wafflegum@users.noreply.github.com>
2025-07-04 05:50:49 +0800

    Refactor CSS styles to remove light-dark function and improve theme handling; add TimerDone component

---
+++ src/components/TimerDone.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TimerDone() {
+	return <div>TimerDone</div>;
+}
+
+export default TimerDone;
--- src/routes/Settings.jsx
+++ src/routes/Settings.jsx
@@ -11,6 +11,13 @@ import "../css/Settings.css";
 function Settings() {
 	const [version, setVersion] = useState("");
 
+	function handleDarkMode() {
+		document.querySelector("body").setAttribute("theme", "dark");
+	}
+	function handleLightMode() {
+		document.querySelector("body").setAttribute("theme", "light");
+	}
+
 	useEffect(() => {
 		async function fetchAppInfo() {
 			if (window.__TAURI__) {
@@ -29,28 +36,40 @@ function Settings() {
 		<div className="page-container" id="settingsPage">
 			<div className="page-title">Settings</div>
 			<div className="settings-container">
+				{/* Timer Settings */}
+				{/* <div className="settings-container__item">
+					<div className="setting-title">Timer</div>
+					<div className="settings-option">
+						<div>Confirmation on finish</div>
+						<input type="checkbox" />
+					</div>
+					<div className="settings-option">
+						<div></div>
+					</div>
+				</div> */}
+
 				{/* Appearance */}
 				<div className="settings-container__item">
 					<div className="setting-title">Appearance</div>
 					<div className="appearance-options-container">
-						<div className="appearance-option">
+						<div className="appearance-option" onClick={() => handleLightMode()}>
 							<span className="icon">
 								<FontAwesomeIcon icon={faSun} />
 							</span>
 							<span>Light</span>
 						</div>
-						<div className="appearance-option">
+						<div className="appearance-option" onClick={() => handleDarkMode()}>
 							<span className="icon">
 								<FontAwesomeIcon icon={faMoon} />
 							</span>
 							<span>Dark</span>
 						</div>
-						<div className="appearance-option">
+						{/* <div className="appearance-option">
 							<span className="icon">
 								<FontAwesomeIcon icon={faMobileScreen} />
 							</span>
 							<span>System</span>
-						</div>
+						</div> */}
 					</div>
 
 					<div className="settings-option">
